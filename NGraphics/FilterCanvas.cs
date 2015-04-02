8f6eee8e737272d410a104cba19f11b8687e9f15
Frank A. Krueger <fak@praeclarum.org>
2015-04-02 14:10:55 -0700

    Merge StyleFilter into Filter

--- NGraphics/FilterCanvas.cs
+++ NGraphics/FilterCanvas.cs
@@ -13,6 +13,16 @@ namespace NGraphics
 			NextCanvas = nextCanvas;
 		}
 
+		public virtual Pen GetPen (Pen pen)
+		{
+			return pen;
+		}
+
+		public virtual Brush GetBrush (Brush brush)
+		{
+			return brush;
+		}
+
 		#region ICanvas implementation
 
 		public virtual void SaveState ()
@@ -32,22 +42,22 @@ namespace NGraphics
 
 		public virtual void DrawText (string text, Rect frame, Font font, TextAlignment alignment = TextAlignment.Left, Pen pen = null, Brush brush = null)
 		{
-			NextCanvas.DrawText (text, frame, font, alignment, pen, brush);
+			NextCanvas.DrawText (text, frame, font, alignment, GetPen (pen), GetBrush (brush));
 		}
 
 		public virtual void DrawPath (System.Collections.Generic.IEnumerable<PathOp> ops, Pen pen = null, Brush brush = null)
 		{
-			NextCanvas.DrawPath (ops, pen, brush);
+			NextCanvas.DrawPath (ops, GetPen (pen), GetBrush (brush));
 		}
 
 		public virtual void DrawRectangle (Rect frame, Pen pen = null, Brush brush = null)
 		{
-			NextCanvas.DrawRectangle (frame, pen, brush);
+			NextCanvas.DrawRectangle (frame, GetPen (pen), GetBrush (brush));
 		}
 
 		public virtual void DrawEllipse (Rect frame, Pen pen = null, Brush brush = null)
 		{
-			NextCanvas.DrawEllipse (frame, pen, brush);
+			NextCanvas.DrawEllipse (frame, GetPen (pen), GetBrush (brush));
 		}
 
 		public virtual void DrawImage (IImage image, Rect frame, double alpha = 1.0)
@@ -56,39 +66,6 @@ namespace NGraphics
 		}
 
 		#endregion
-	}
-
-	public class StyleFilterCanvas : FilterCanvas
-	{
-		public StyleFilterCanvas (ICanvas nextCanvas)
-			: base (nextCanvas)
-		{
-			
-		}
-		public virtual Pen GetPen (Pen pen)
-		{
-			return pen;
-		}
-		public virtual Brush GetBrush (Brush brush)
-		{
-			return brush;
-		}
-		public override void DrawPath (System.Collections.Generic.IEnumerable<PathOp> ops, Pen pen, Brush brush)
-		{
-			base.DrawPath (ops, GetPen (pen), GetBrush (brush));
-		}
-		public override void DrawRectangle (Rect frame, Pen pen, Brush brush)
-		{
-			base.DrawRectangle (frame, GetPen (pen), GetBrush (brush));
-		}
-		public override void DrawEllipse (Rect frame, Pen pen, Brush brush)
-		{
-			base.DrawEllipse (frame, GetPen (pen), GetBrush (brush));
-		}
-		public override void DrawText (string text, Rect frame, Font font, TextAlignment alignment = TextAlignment.Left, Pen pen = null, Brush brush = null)
-		{
-			base.DrawText (text, frame, font, alignment, GetPen (pen), GetBrush (brush));
-		}
 	}
 }
 
