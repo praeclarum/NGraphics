977c7075bde11077ca78f0afd0c3c854592c7af4
Frank A. Krueger <fak@praeclarum.org>
2015-12-17 14:33:34 -0800

    Fix SVG view box output

--- NGraphics/SvgWriter.cs
+++ NGraphics/SvgWriter.cs
@@ -33,7 +33,7 @@ namespace NGraphics
 			w.WriteLine ("<?xml version=\"1.0\" encoding=\"{0}\" standalone=\"no\"?>", w.Encoding.WebName.ToUpperInvariant ());
 			w.WriteLine ("<svg width=\"{0}px\" height=\"{1}px\" viewBox=\"{2} {3} {4} {5}\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\">",
 				Graphic.Size.Width, Graphic.Size.Height,
-				Graphic.ViewBox.Y, Graphic.ViewBox.X,
+				Graphic.ViewBox.X, Graphic.ViewBox.Y,
 				Graphic.ViewBox.Width, Graphic.ViewBox.Height);
 			w.Indent ();
 			w.WriteLine ("<title>{0}</title>", Escape (Graphic.Title));
