f276fef5ad04dc74329bb0041612615beb288717
Matthew Little <matt@pingman.com>
2017-10-13 18:55:51 -0600

    Update NGraphics.UWP csproj to build nuget packages referencing core package, revert rename NGraphics.Core folder back to NGraphics to follow orig repo more closely

--- Platforms/NGraphics.UWP/Platforms.cs
+++ Platforms/NGraphics.UWP/Platforms.cs
@@ -5,37 +5,6 @@ using System.Threading.Tasks;
 
 namespace NGraphics.UWP
 {
-	public static class Platforms
-	{
-		static readonly IPlatform nulll = new NullPlatform ();
-		static IPlatform current = null;
-
-		public static IPlatform Null { get { return nulll; } }
-
-		public static IPlatform Current {
-			get {
-				if (current == null) {
-					#if MAC
-					current = new ApplePlatform ();
-					#elif __IOS__
-					current = new ApplePlatform ();
-					#elif __TVOS__
-					current = new ApplePlatform ();
-					#elif __ANDROID__
-					current = new AndroidPlatform ();
-                    #elif WINDOWS_UWP
-                    current = new WinUniversalPlatform ();
-                    #elif NETFX_CORE
-					current = new WinRTPlatform ();
-					#else
-					current = new SystemDrawingPlatform ();
-					#endif
-				}
-				return current;
-			}
-		}
-	}
-
 	public static class GraphicFilesEx
 	{
 		public static async Task WriteSvgAsync (this Graphic g, string path)
